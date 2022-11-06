ac6d82e95640477f6254133a8d10962a6ce11949
dianafocea <diana.focea13@gmail.com>
2022-11-06 20:59:41 +0200

    feat: tema7

---
+++ tema7.py
@@ -0,0 +1,109 @@
+from abc import ABC, abstractmethod
+
+class FormaGeometrica(ABC):
+    PI = 3.14
+
+    @abstractmethod
+    def aria(self):
+        pass
+
+    @classmethod
+    def descrie(self):
+        print('`Cel mai probabil am colturi`')
+
+
+class Patrat(FormaGeometrica):
+    def __init__(self, latura):
+        self.__latura = latura
+
+    def get_latura(self):
+        return self.__latura
+
+    def set_latura(self, latura):
+        self.__latura = latura
+
+    def delete_latura(self):
+        self.__latura = None
+
+    def aria(self):
+        print(f"latura = {self.__latura}.")
+        if self.__latura == 0 or self.__latura == None:
+            return ("Nu putem calcula aria unui patrat a carui latura = None sau zero!")
+        else:
+            return self.__latura ** 2
+
+    #metoda descrie = metoda de clasa, care se mosteneste automat,
+# fara a fi nevoie sa mai definim clasa descrie in copil
+    def descrie(self):
+        if self.__latura == 0 or self.__latura == None:
+            print("Nu poate fi descrisa o figura geometrica cu latura 0 sau None")
+        else:
+            return super().descrie()
+
+class Cerc(FormaGeometrica):
+    def __init__(self, raza):
+        self.__raza = raza
+
+
+    def get_raza(self):
+        return self.__raza
+
+    def set_raza(self, raza):
+        self.__raza = raza
+
+    def delete_raza(self):
+        self.__raza = None
+
+    def aria(self):
+        print(f"raza = {self.__raza}.")
+        if self.__raza == 0 or self.__raza == None:
+            return (f"Nu putem calcula aria unui cerc a carei raza = None sau zero!")
+        else:
+            return self.PI * (self.__raza ** 2)
+
+    def descrie(self):
+        if self.__raza == 0 or self.__raza == None:
+            print("Nu poate fi descrisa o figura geometrica cu latura 0 sau None")
+        else:
+            print('‘Eu nu am colturi’')
+
+print(" ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ")
+lst_frm_geom = [Patrat(1), Patrat(0), Patrat(None), Cerc(1), Cerc(None), Cerc(0)]
+for f in lst_frm_geom:
+    print(f'aria:  {f.aria()}')
+    f.descrie()
+    print(" ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ` ")
+
+print(" ` ` ` ` ` ` patratul:` ` ` ` ` ` ` ` ` ` ` ` ` ` ")
+p1 = Patrat(5)
+print("aria p1 = ", p1.aria())
+print('descrie p1:')
+p1.descrie()
+print("get_latura p1 = ", p1.get_latura())
+p1.set_latura(2)
+print("get_latura p1 dupa set: ", p1.get_latura())
+print("delete_latura p1 = ", p1.delete_latura())
+print("get_latura dupa delete = ", p1.get_latura())
+print('descrie p1:')
+p1.descrie()
+
+print(" ` ` ` ` ` `cercul: ` ` ` ` ` ` ` ` ` ` ` ` ` ` ")
+
+c1 = Cerc(5)
+print("aria c1 = ", c1.aria())
+print('descrie c1:')
+c1.descrie()
+print("get_raza c1 = ", c1.get_raza())
+c1.set_raza(2)
+print("get_raza c1 dupa set: ", c1.get_raza())
+print("delete_raza c1 = ", c1.delete_raza())
+print("get_raza dupa delete = ", c1.get_raza())
+print('descrie c1:')
+c1.descrie()
+
+
+
+
+
+
+
---
+++ tema7_pythonic.py
@@ -0,0 +1,118 @@
+from abc import ABC, abstractmethod
+
+class FormaGeometrica(ABC):
+    PI = 3.14
+
+    @abstractmethod
+    def aria(self):
+        pass
+
+    @classmethod
+    def descrie(self):
+        print('`Cel mai probabil am colturi`')
+
+print("PYthonic encaps:")
+
+class PatratPy(FormaGeometrica):
+    def __init__(self, latura):
+        self.__latura = latura
+
+    @property
+    def latura(self):
+        return self.__latura
+
+    def aria(self):
+        print(f"latura = {self.__latura}.")
+        if self.__latura == 0 or self.__latura == None:
+            return ("Nu putem calcula aria unui patrat a carui latura = None sau zero!")
+        else:
+            return self.__latura ** 2
+
+    def descrie(self):
+        if self.__latura == 0 or self.__latura == None:
+            print("Nu poate fi descrisa o figura geometrica cu latura 0 sau None")
+        else:
+            super().descrie()
+
+    @latura.getter
+    def latura(self):
+        return self.__latura
+
+    @latura.setter
+    def latura(self, val):
+        self.__latura = val
+
+    @latura.deleter
+    def latura(self):
+        self.__latura = None
+
+
+class CercPy(FormaGeometrica):
+    def __init__(self, raza):
+        self.__raza = raza
+
+    @property
+    def raza(self):
+        return self.__raza
+
+    def aria(self):
+        print(f"raza = {self.__raza}.")
+        if self.__raza == 0 or self.__raza == None:
+            return ("Nu putem calcula aria unui cerc a carei raza = None sau zero!")
+        else:
+            return self.__raza * (self.__raza ** 2)
+
+    def descrie(self):
+        if self.__raza == 0 or self.__raza == None:
+            print("Nu poate fi descrisa o figura geometrica cu raza 0 sau None")
+        else:
+            print('‘Eu nu am colturi’')
+
+
+    @raza.getter
+    def raza(self):
+        return self.__raza
+
+    @raza.setter
+    def raza(self, val):
+        self.__raza = val
+
+    @raza.deleter
+    def raza(self):
+        self.__raza = None
+
+
+print('`````````````````````````````````````````')
+
+p1 = PatratPy(5)
+print(f"descriere patrat: ")
+p1.descrie()
+print(f'aria patratului cu latura = {p1.latura}, aria este: {p1.aria()}')
+print(f"latura patratului inainte de setter este: {p1.latura}")
+p1.latura = 8
+print(f'aria patratului dupa setter cu latura {p1.latura} = {p1.aria()}')
+del p1.latura
+print(f"latura dupa stergere: {p1.latura}")
+p1.latura = 1
+print(f"pt latura = {p1.latura}, aria este: {p1.aria()}")
+p1.descrie()
+del p1.latura
+print(f"pt latura = {p1.latura}, aria este: {p1.aria()}")
+p1.descrie()
+
+print('`````````````````````````````````````````')
+
+c1 = CercPy(5)
+c1.descrie()
+print(f'aria cercului cu raza = {c1.raza}, aria este: {c1.aria()}')
+print(f"raza cercului inainte de setter este: {c1.raza}")
+c1.raza = 8
+print(f'aria cercului dupa setter cu raza {c1.raza} = {c1.aria()}')
+del c1.raza
+print(f"raza dupa stergere: {c1.raza}")
+c1.raza = 1
+print(f"pt raza = {c1.raza}, aria este: {c1.aria()}")
+c1.descrie()
+del c1.raza
+print(f"pt raza = {c1.raza}, aria este: {c1.aria()}")
+c1.descrie()
